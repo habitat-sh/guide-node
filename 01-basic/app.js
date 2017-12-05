f2d19e53e365896daa8d8609c5c51c15901257a4
Garrett Amini <kadjar@gmail.com>
2017-12-05 13:31:38 -0500

    adding start of the finish
    Signed-off-by: Garrett Amini <kadjar@gmail.com>

--- 01-basic/app.js
+++ 01-basic/app.js
@@ -1,14 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const cfg = JSON.parse(fs.readFileSync(process.env.APP_CONFIG || './config-default.json', 'utf8'));
+
+const host = cfg.srv.host;
+const port = cfg.srv.port;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end(`Hello World! I am running at ${host} on port ${port}\n`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
 });
---
+++ 01-basic/habitat/README.md
@@ -0,0 +1,9 @@
+# Habitat package: 01-basic
+
+## Description
+
+Provide a brief description of the `01-basic` plan / purpose.
+
+## Usage
+
+Describe the general usage for the `01-basic` plan
