1e4d3dca71b6530f4cc94735be349377b32affcc
Garrett Amini <kadjar@gmail.com>
2017-12-06 18:03:30 -0500

    adding 03
    Signed-off-by: Garrett Amini <kadjar@gmail.com>

--- 02-basic-scaffolding/app.js
+++ 02-basic-scaffolding/app.js
@@ -1,14 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
-const host = '127.0.0.1';
-const port = 3000;
+const cfg = JSON.parse(fs.readFileSync(process.env.APP_CONFIG || '../config/config.json', 'utf8'));
+
+const message = cfg.message;
+const port = cfg.port;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end(`Hello World! ${message}!  It's running on port ${port}\n`);
 });
 
-server.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}/`);
+server.listen(port, function() {
+  console.log(`Server running at http://0.0.0.0:%d`, port);
 });
--- 02-basic-scaffolding/habitat/plan.sh
+++ 02-basic-scaffolding/habitat/plan.sh
@@ -3,3 +3,8 @@ pkg_origin=mgamini
 pkg_version="0.1.0"
 pkg_scaffolding="core/scaffolding-node"
 
+
+declare -A scaffolding_env
+
+# Define path to config file
+scaffolding_env[APP_CONFIG]="{{pkg.svc_config_path}}/config.json"
